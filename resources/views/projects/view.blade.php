aae6bd4b3dd9dd6af08ecb0c83aa9f6dd9fe8663
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2023-08-25 00:07:04 +0530

    change the project and task view design

--- resources/views/projects/view.blade.php
+++ resources/views/projects/view.blade.php
@@ -7,54 +7,71 @@
             <div class="p-6 text-gray-900">
             <a href="{{ route('projects.index') }}" title="back" class="inline-flex items-center px-4 py-2 bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-900 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition ease-in-out duration-150" ><i class="fa-solid fa-arrow-left-long"></i></a><br><br>
                 <br>
-                <h1 class="text-left text-xl text-gray-700">Project Details</h1><br>
-                <p class="text-center text-xl text-gray-700">Project Title: <strong class="text-emerald-600">{{ $project->title }}</strong></p>
-                <p class="font-bold text-5xl text-emerald-600"><span class="text-sm text-gray-700">ID:</span> #{{ $project->id }}</p><br>
-                <div class="flex justify-between">
-                    <p class="text-xl text-gray-700">Start Date: <span class="font-bold">{{ $project->start_date }}</span></p>
-                    <p class="text-xl text-gray-700">Due Date: 
-                        <span class="font-bold">
-                            {{ $project->due_date }} 
-                            @php
-                                use Carbon\Carbon;
+                <h1 class="text-center text-xl text-gray-700">Project Details</h1>
+                <p class="text-left text-xl text-gray-700 font-bold">{{ $project->title }}</p><br>
+                <div class="py-5 bg-gray-100 px-5 rounded-lg">                    
+                    <div class="flex justify-between">
+                        <div>
+                            <p class="text-base font-bold text-gray-700">Project Overview</p>
+                            <p class="font-bold text-2xl text-emerald-600"><span class="text-sm text-gray-700">ID:</span> #{{ $project->id }}</p>
+                            @if($project->note != null)
+                                <p class="text-gray-700">Note: {{ $project->note }}</p>
+                            @endif
+                        </div>
+                        <div>
+                            <p class="text-sm text-gray-700 font-bold">Start Date <br>
+                                {{ $project->start_date }}
+                            </p><br>
+                            <div class="flex justify-between">
+                                <div>
+                                    <p class="text-sm text-gray-700 font-bold">Due Date <br>
+                                        {{ $project->due_date }}
+                                    </p>
+                                </div>
+                                <div>&nbsp;&nbsp;&nbsp;</div>
+                                <div>
+                                    <p class="text-4xl text-gray-700 font-bold">
+                                        @php
+                                            use Carbon\Carbon;
 
-                                $start = Carbon::parse(date("Y-m-d"));
-                                $due = Carbon::parse($project->due_date);
+                                            $start = Carbon::parse(date("Y-m-d"));
+                                            $due = Carbon::parse($project->due_date);
 
-                                $diff = $due->diffInDays($start);
-                                if ($diff < 0 || $due < $start) {
-                                    $diff = 0;
-                                }
-                            @endphp
-                            ({{ $diff }} days left)
-                        </span>
-                    </p>
-                </div><br>                
+                                            $diff = $due->diffInDays($start);
+                                            if ($diff < 0 || $due < $start) {
+                                                $diff = 0;
+                                            }
+                                        @endphp
+                                        {{ $diff }} days left
+                                    </p>
+                                </div>
+                            </div>                            
+                        </div>
+                    </div> 
+                </div><br>                               
                 @if($project->user_ids != null)
-                <p class="text-gray-700">Users:  
+                <p class="text-gray-700">Users</p>
+                <div class="py-5 bg-gray-100 px-5 rounded-lg w-96">                    
                     @php
                         $user_ids = json_decode($project->user_ids);
                     @endphp
                     @for ($i = 0; $i < count($user_ids); $i++)
-                        {{ App\Models\User::find($user_ids[$i])->fname }} {{ App\Models\User::find($user_ids[$i])->lname }} ({{ App\Models\User::find($user_ids[$i])->username }}),
-                    @endfor 
-                </p>
+                    <p class="text-gray-700">{{ App\Models\User::find($user_ids[$i])->fname }} {{ App\Models\User::find($user_ids[$i])->lname }} ({{ App\Models\User::find($user_ids[$i])->username }})</p>
+                    @endfor
+                </div><br>
                 @endif
-                
-                @if($project->note != null)
-                    <p class="text-gray-700">Note: {{ $project->note }}</p>
-                @endif
-                <br>
-                <div class="flex justify-between">
-                    <div>
-                        <p class="text-gray-700">Add By: {{ $project->addby->username }}</p> 
-                        <p class="text-gray-700">Add Date: {{ $project->created_at->format('Y-m-d') }}</p>
-                    </div>
-                    <div>
-                        <p class="text-gray-700">Last Edit By: {{ $project->editby->username }}</p> 
-                        <p class="text-gray-700">Last Edit Date: {{ $project->updated_at->format('Y-m-d') }}</p>
-                    </div>
-                </div>                                                        
+                <div class="py-5 bg-gray-100 px-5 rounded-lg">                    
+                    <div class="flex justify-between">
+                        <div>
+                            <p class="text-gray-700">Add By: {{ $project->addby->username }}</p> 
+                            <p class="text-gray-700">Add Date: {{ $project->created_at->format('Y-m-d') }}</p>
+                        </div>
+                        <div>
+                            <p class="text-gray-700">Last Edit By: {{ $project->editby->username }}</p> 
+                            <p class="text-gray-700">Last Edit Date: {{ $project->updated_at->format('Y-m-d') }}</p>
+                        </div>
+                    </div> 
+                </div><br>                                                                       
             </div>
         </div>
     </div>
--- resources/views/tasks/view.blade.php
+++ resources/views/tasks/view.blade.php
@@ -7,46 +7,65 @@
             <div class="p-6 text-gray-900">
             <a href="{{ route('tasks.index') }}" title="back" class="inline-flex items-center px-4 py-2 bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-900 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition ease-in-out duration-150" ><i class="fa-solid fa-arrow-left-long"></i></a><br><br>
                 <br>
-                <h1 class="text-left text-xl text-gray-700">Task Details</h1><br>
-                <p class="text-center text-xl text-gray-700">Task Title: <strong class="text-emerald-600">{{ $task->title }}</strong></p>
-                <p class="font-bold text-5xl text-emerald-600"><span class="text-sm text-gray-700">ID:</span> #{{ $task->id }}</p><br>
-                <div class="flex justify-between">
-                    <p class="text-xl text-gray-700">Start Date: <span class="font-bold">{{ $task->start_date }}</span></p>
-                    <p class="text-xl text-gray-700">Due Date: 
-                        <span class="font-bold">
-                            {{ $task->due_date }} 
-                            @php
-                                use Carbon\Carbon;
+                <h1 class="text-center text-xl text-gray-700">Task Details</h1>
+                <p class="text-left text-xl text-gray-700 font-bold">{{ $task->title }}</p><br>
+                <div class="py-5 bg-gray-100 px-5 rounded-lg">                    
+                    <div class="flex justify-between">
+                        <div>
+                            <p class="text-base font-bold text-gray-700">Task Overview</p>
+                            <p class="font-bold text-2xl text-emerald-600"><span class="text-sm text-gray-700">ID:</span> #{{ $task->id }}</p>
+                            @if($task->note != null)
+                                <p class="text-gray-700">Note: {{ $task->note }}</p>
+                            @endif
+                        </div>
+                        <div>
+                            <p class="text-sm text-gray-700 font-bold">Start Date <br>
+                                {{ $task->start_date }}
+                            </p><br>
+                            <div class="flex justify-between">
+                                <div>
+                                    <p class="text-sm text-gray-700 font-bold">Due Date <br>
+                                        {{ $task->due_date }}
+                                    </p>
+                                </div>
+                                <div>&nbsp;&nbsp;&nbsp;</div>
+                                <div>
+                                    <p class="text-4xl text-gray-700 font-bold">
+                                        @php
+                                            use Carbon\Carbon;
 
-                                $start = Carbon::parse(date("Y-m-d"));
-                                $due = Carbon::parse($task->due_date);
+                                            $start = Carbon::parse(date("Y-m-d"));
+                                            $due = Carbon::parse($task->due_date);
 
-                                $diff = $due->diffInDays($start);
-                                if ($diff < 0 || $due < $start) {
-                                    $diff = 0;
-                                }
-                            @endphp
-                            ({{ $diff }} days left)
-                        </span>
-                    </p>
-                </div><br> 
-                <p class="text-gray-700">Project: {{ $task->project->title }}</p>
-                <p class="text-gray-700">User:  {{ $task->user->fname }} {{ $task->user->lname }} ({{ $task->user->username }})</p>
-                <p class="text-gray-700">Status: {{ $task->status }}</p>
-                @if($task->note != null)
-                    <p class="text-gray-700">Note: {{ $task->note }}</p>
-                @endif
-                <br>
-                <div class="flex justify-between">
-                    <div>
-                        <p class="text-gray-700">Add By: {{ $task->addby->username }}</p> 
-                        <p class="text-gray-700">Add Date: {{ $task->created_at->format('Y-m-d') }}</p>
-                    </div>
-                    <div>
-                        <p class="text-gray-700">Last Edit By: {{ $task->editby->username }}</p> 
-                        <p class="text-gray-700">Last Edit Date: {{ $task->updated_at->format('Y-m-d') }}</p>
-                    </div>
-                </div>                        
+                                            $diff = $due->diffInDays($start);
+                                            if ($diff < 0 || $due < $start) {
+                                                $diff = 0;
+                                            }
+                                        @endphp
+                                        {{ $diff }} days left
+                                    </p>
+                                </div>
+                            </div>                            
+                        </div>
+                    </div> 
+                </div><br>
+                <div class="py-5 bg-gray-100 px-5 rounded-lg w-96">                    
+                    <p class="text-gray-700">Project: {{ $task->project->title }}</p>
+                    <p class="text-gray-700">User:  {{ $task->user->fname }} {{ $task->user->lname }} ({{ $task->user->username }})</p>
+                    <p class="text-gray-700">Status: {{ $task->status }}</p>
+                </div><br>
+                <div class="py-5 bg-gray-100 px-5 rounded-lg">                    
+                    <div class="flex justify-between">
+                        <div>
+                            <p class="text-gray-700">Add By: {{ $task->addby->username }}</p> 
+                            <p class="text-gray-700">Add Date: {{ $task->created_at->format('Y-m-d') }}</p>
+                        </div>
+                        <div>
+                            <p class="text-gray-700">Last Edit By: {{ $task->editby->username }}</p> 
+                            <p class="text-gray-700">Last Edit Date: {{ $task->updated_at->format('Y-m-d') }}</p>
+                        </div>
+                    </div> 
+                </div><br>                       
             </div>
         </div>
     </div>
